89bf7ef8d95f28d6d52aa95b669a3ad25eae6ffb
Ioan Moldovan <ioanmoldovan95@gmail.com>
2020-11-23 10:12:05 +0200

    First commit

---
+++ app/src/main/java/com/example/revdemoapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.revdemoapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.revdemoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
